5fb02f70c2c2aeb4059fed7dc4b37cedace2c057
dinisgomes98 <dofen98@gmail.com>
2026-06-13 00:56:05 +0100

    main running

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from api.routes.study_session import session_router
+from api.routes.subject import subject_router
+from api.database import Base, engine
+from api.models.study_session import StudySession
+from api.models.subject import Subject
 
 app = FastAPI()
+
+app.include_router(session_router)
+app.include_router(subject_router)
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def index():
